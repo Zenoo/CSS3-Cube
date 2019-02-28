763fab8c4d70da89fbcf3397a2d9ae9da139b305
Zenoo <fzenooo@gmail.com>
2019-02-28 10:53:55 +0100

    Mouse interaction done | fix #2

--- Cube.js
+++ Cube.js
@@ -14,7 +14,10 @@ class Cube{
 		 */
 		this.parent = target instanceof Element ? target : document.querySelector(target);
 
-		this._parameters = parameters;
+		this._parameters = {
+			ratio: 2,
+			...parameters
+		};
 
 		this._build();
 		this._listen();
@@ -27,7 +30,7 @@ class Cube{
 	_build(){
 		this.parent.classList.add('css3-cube-wrapper');
 		this.parent.innerHTML = `
-			<div class="css3-cube">
+			<div class="css3-cube" style="transform: translateZ(-50px) rotateX(0deg) rotateY(0deg);">
 				<header class="cube-face" data-face="top"></header>
 				<section class="cube-face" data-face="front"></section>
 				<aside class="cube-face" data-face="right"></aside>
@@ -36,6 +39,8 @@ class Cube{
 				<footer class="cube-face" data-face="bottom"></footer>
 			</div>
 		`;
+
+		this.element = this.parent.firstElementChild;
 	}
 
 	/**
@@ -43,6 +48,59 @@ class Cube{
 	 * @private
 	 */
 	_listen(){
+		const origin = {
+			x: 0,
+			y: 0
+		},
+		rotation = {
+			x: 0,
+			y: 0
+		};
+		let dragging = false;
 
+		// Start events
+		const handleMousedown = e => {
+			dragging = true;
+
+			// Remember the drag origin
+			origin.x = e.pageX;
+			origin.y = e.pageY;
+
+			// Remember the drag original rotation
+			rotation.x = +this.element.style.transform.replace(/^.*rotateX\((-?\d+)deg\).*$/u, '$1');
+			rotation.y = +this.element.style.transform.replace(/^.*rotateY\((-?\d+)deg\).*$/u, '$1');
+
+			console.log(rotation);
+		};
+		
+		this.parent.addEventListener('mousedown', handleMousedown);
+		this.parent.addEventListener('dragstart', handleMousedown);
+
+		// Move events
+		const handleMouseMove = e => {
+			if(dragging){
+				const mouseDifference = {
+					x: (e.pageX - origin.x) * this._parameters.ratio,
+					y: (e.pageY - origin.y) * this._parameters.ratio
+				};
+
+				this.element.style.transform = `translateZ(-50px) rotateX(${rotation.x - mouseDifference.y}deg) rotateY(${rotation.y + mouseDifference.x}deg)`;
+			}
+		};
+
+		document.addEventListener('mousemove', handleMouseMove);
+		document.addEventListener('dragover', handleMouseMove);
+
+		// End events
+		const handleMouseUp = () => {
+			dragging = false;
+		};
+
+		this.element.addEventListener('dragend', handleMouseUp);
+		window.addEventListener('mouseup', handleMouseUp);
+
+		window.addEventListener('mouseout', e => {
+			if(e.relatedTarget === null) dragging = false;
+		});
 	}
 }
